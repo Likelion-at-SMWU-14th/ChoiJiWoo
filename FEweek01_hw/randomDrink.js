2a726d7b1c729a169a942f36d0070df3e289e01f
jiwoo choi <chlwldn418@naver.com>
2026-04-11 18:21:38 +0900

    Feat : 음료 배열 추가

---
+++ FEweek01_hw/randomDrink.js
@@ -0,0 +1,11 @@
+const drinks = [
+    { name: "코카콜라", price: 1500 },
+    { name: "칠성사이다", price: 1400 },
+    { name: "알로에", price: 1600 },
+    { name: "포카리스웨트", price: 1700 },
+    { name: "갈아만든 배", price: 1200 },
+    { name: "비타500", price: 500 },
+    { name: "카페라떼", price: 2000 },
+    { name: "데미소다", price: 1300 },
+    { name: "바나나우유", price: 1800 },
+    { name: "레몬에이드", price: 1700 }];
