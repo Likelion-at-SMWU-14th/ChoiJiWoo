6a3c3d72126d2fd87ea9c01fa9acbbe42b7db38d
jiwoo choi <chlwldn418@naver.com>
2026-06-28 01:46:13 +0900

    feat: ProductCard 렌더링 및 이미지 적용

--- front/week05_hw/src/App.jsx
+++ front/week05_hw/src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import ProductCard from "./components/ProductCard";
+import productImage1 from "./assets/아틀리에 나인.webp";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ProductCard
+        rank={1}
+        brand="아틀리에 나인"
+        description="[5차] [제제로 PICK] POINTEL SLIMFIT ROUN..."
+        discountRate={40}
+        price={23400}
+        tags={["쿠폰", "조건부 무료배송"]}
+        image={productImage1}
+      />
+    </>
+  );
 }
 
 export default App;
