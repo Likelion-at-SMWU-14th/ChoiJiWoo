5d2be4f03fc873ccb2e49b92ba8fe87660236a5b
jiwoo choi <chlwldn418@naver.com>
2026-06-30 16:29:26 +0900

    feat : useEffect 의존성 배열 실습

--- front/week06/src/App.jsx
+++ front/week06/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import UserInfo from "./components/Info";
+import DebouncedSearch from "./components/DebouncedSearch";
+
 function App() {
   return (
     <div>
       <UserInfo />
+      <DebouncedSearch />
     </div>
   );
 }
