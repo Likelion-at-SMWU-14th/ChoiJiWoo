c6652390c498e1e540c5b26a29d4a1bf0ba5211f
jiwoo choi <chlwldn418@naver.com>
2026-07-14 16:54:36 +0900

    feat: 전체 게시글 조회 및 API 연동 완료

--- front/week08/src/router.jsx
+++ front/week08/src/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import CommentPate from "./pages/CommentPage";
+import CommentPage from "./pages/CommentPage";
 import DetailPage from "./pages/DetailPage";
 import WritePage from "./pages/WritePage";
 
