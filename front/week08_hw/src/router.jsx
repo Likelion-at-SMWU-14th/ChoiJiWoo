068f59433e700522bcfd3eb581f6f5995f6bda0d
jiwoo choi <chlwldn418@naver.com>
2026-07-19 04:36:11 +0900

    feat: 특정 게시글 조회 API 연동

--- front/week08_hw/src/router.jsx
+++ front/week08_hw/src/router.jsx
@@ -1,9 +1,27 @@
-import { createBrouserRouter } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import CommentPage from "./pages/CommentPage";
+import DetailPage from "./pages/DetailPage";
+import WritePage from "./pages/WritePage";
 
-const router = createBrouserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <CommentPage />,
+      },
+      {
+        path: "detail/:id",
+        element: <DetailPage />,
+      },
+      {
+        path: "write",
+        element: <WritePage />,
+      },
+    ],
   },
 ]);
 
