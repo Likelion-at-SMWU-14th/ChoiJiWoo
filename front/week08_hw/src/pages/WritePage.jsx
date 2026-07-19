428c3d4c74d9a26d12f7124c7f3e06367c0c0610
jiwoo choi <chlwldn418@naver.com>
2026-07-20 00:58:06 +0900

    feat: 게시글 수정 api연동

--- front/week08_hw/src/pages/WritePage.jsx
+++ front/week08_hw/src/pages/WritePage.jsx
@@ -2,36 +2,86 @@ import React from "react";
 import Button from "../components/Button";
 import CommentForm from "../components/CommentForm";
 import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const WritePage = () => {
   const navigate = useNavigate();
+  const { id } = useParams();
+  const isEdit = Boolean(id);
   const [author, setAuthor] = useState("");
   const [comment, setComment] = useState("");
+  const [isLoading, setIsLoading] = useState(isEdit);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const baseURL = import.meta.env.VITE_API_BASE_URL;
-  const postComment = () => {
+
+  useEffect(() => {
+    if (!isEdit) return;
+
+    setIsLoading(true);
     axios
-      .post("http://127.0.0.1:8000/entries/", {
-        author: author,
-        comment: comment,
-      })
+      .get(`${baseURL}/entries/${id}/`)
       .then((res) => {
-        alert("게시글 작성이 완료되었어요 !!");
-        console.log("게시글 작성이 완료되었습니다.");
+        setAuthor(res.data.author);
+        setComment(res.data.comment);
       })
       .catch((err) => {
         console.log(err);
-        alert("게시글 작성에 실패했어요 .. ㅠ");
+        alert("게시글을 불러오지 못했습니다.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [id, isEdit, baseURL]);
+
+  const postComment = () => {
+    if (isSubmitting) return;
+    setIsSubmitting(true);
+
+    const request = isEdit
+      ? axios.put(`${baseURL}/entries/${id}/`, { author, comment })
+      : axios.post(`${baseURL}/entries/`, { author, comment });
+
+    request
+      .then(() => {
+        alert(
+          isEdit
+            ? "게시글 수정이 완료되었어요 !!"
+            : "게시글 작성이 완료되었어요 !!",
+        );
+        navigate(-1);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(
+          isEdit
+            ? "게시글 수정에 실패했어요 .. ㅠ"
+            : "게시글 작성에 실패했어요 .. ㅠ",
+        );
+      })
+      .finally(() => {
+        setIsSubmitting(false);
       });
   };
 
+  if (isLoading) {
+    return <WritePageWrapper>불러오는 중...</WritePageWrapper>;
+  }
+
   return (
     <WritePageWrapper>
-      <CommentForm setAuthor={setAuthor} setComment={setComment} />
+      <CommentForm
+        author={author}
+        comment={comment}
+        setAuthor={setAuthor}
+        setComment={setComment}
+      />
       <ButtonWrapper>
-        <Button text="작성하기" onBtnClick={postComment} />
+        <Button
+          text={isEdit ? "수정하기" : "작성하기"}
+          onBtnClick={postComment}
+        />
         <Button text="취소" onBtnClick={() => navigate(-1)} />
       </ButtonWrapper>
     </WritePageWrapper>
