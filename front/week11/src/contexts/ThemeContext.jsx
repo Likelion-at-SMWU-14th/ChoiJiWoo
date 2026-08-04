b94cf7af11d9ff5382d26d102b92732b8f73cac3
jiwoo choi <chlwldn418@naver.com>
2026-08-04 15:57:57 +0900

    feat: ThemeProvider 컴포넌트 생성

---
+++ front/week11/src/contexts/ThemeContext.jsx
@@ -0,0 +1,19 @@
+import { Children, createContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeContextProvider = ({ Children }) => {
+  const [isDark, setIsDark] = useState(false);
+  const toggleTheme = () => {
+    setIsDark((prev) => !prev);
+    console.log("테마가 변경되었습니다.");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
