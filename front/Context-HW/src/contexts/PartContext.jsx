0ff5462f14dfe5300a245a2a9485792966207cdd
jiwoo choi <chlwldn418@naver.com>
2026-08-09 01:52:36 +0900

    feat: PartProvider 컴포넌트 생성

--- front/Context-HW/src/contexts/PartContext.jsx
+++ front/Context-HW/src/contexts/PartContext.jsx
@@ -2,4 +2,13 @@ import { createContext } from "react";
 
 const PartContext = createContext();
 
+export function PartProvider({ children }) {
+  const [part, setPart] = useState("");
+
+  return (
+    <PartContext.Provider value={{ part, setPart }}>
+      {children}
+    </PartContext.Provider>
+  );
+}
 export default PartContext;
