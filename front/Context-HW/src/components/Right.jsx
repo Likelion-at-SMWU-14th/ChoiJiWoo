d97cb95d0c656495d6c9eeab86f6ad9880e11b94
jiwoo choi <chlwldn418@naver.com>
2026-08-09 15:41:35 +0900

    feat: Context API를 활용한 파트 상태 관리

--- front/Context-HW/src/components/Right.jsx
+++ front/Context-HW/src/components/Right.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { styled } from "styled-components";
 import MemberList from "./MemberList";
+import { useContext } from "react";
+import PartContext from "../contexts/PartContext";
 
-const Right = ({ part }) => {
+const Right = () => {
+  const { part } = useContext(PartContext);
   return (
     <Wrapper>
       <MemberList part={part} />
