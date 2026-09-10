9bc398003aa8f570b2d42023ac694469291c64ab
jiwoo choi <chlwldn418@naver.com>
2026-09-10 13:30:33 +0900

    feat: APP.jsx 작성

--- front/week14_hw/src/App.jsx
+++ front/week14_hw/src/App.jsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import SignUpForm from "./components/SignUpForm";
+import MyPage from "./components/MyPage";
+import ProfileEditForm from "./components/ProfileEditForm";
+import DeleteAccountButton from "./components/DeleteAccountButton";
 
 function App() {
-  return <></>;
+  const userId = 1;
+  return (
+    <div>
+      <h1>회원 관리</h1>
+      <SignUpForm />
+      <MyPage userId={userId} />
+      <ProfileEditForm userId={userId} />
+      <DeleteAccountButton userId={userId} />
+    </div>
+  );
 }
 
 export default App;
