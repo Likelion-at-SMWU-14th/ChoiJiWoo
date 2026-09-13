84d3be2ac1a01d8d2697e5d6abb9c2b466a5a795
jiwoo choi <chlwldn418@naver.com>
2026-09-13 22:07:14 +0900

    refactor: users API를 axios 대신 mock함수로 변경

--- front/week14_hw/src/api/users.js
+++ front/week14_hw/src/api/users.js
@@ -1,27 +1,37 @@
-import axios from "axios"
-
-const BASE_URL = "/api"
-
-// Create
+// 회원가입
 export const signUp = async ({ username, password }) => {
-  const { data } = await axios.post(`${BASE_URL}/users`, { username, password });
-  return data
-}
+  console.log("POST /api/users", { username, password });
 
-// Read
+  return {
+    id: 1,
+    username,
+  };
+};
+
+// 마이페이지 조회
 export const fetchMyPage = async (userId) => {
-  const { data } = await axios.get(`${BASE_URL}/users/${userId}`);
-  return data
-}
+  console.log(`GET /api/users/${userId}`);
 
-// Update
+  return {
+    id: userId,
+    username: "지우",
+    email: "example@test.com",
+  };
+};
+
+// 개인정보 수정
 export const updateProfile = async ({ userId, ...updatedFields }) => {
-  const { data } = await axios.put(`${BASE_URL}/users/${userId}`, updatedFields);
-  return data
-}
+  console.log(`PUT /api/users/${userId}`, updatedFields);
 
-// Delete
+  return {
+    id: userId,
+    ...updatedFields,
+  };
+};
+
+// 회원 탈퇴
 export const deleteUser = async (userId) => {
-  await axios.delete(`${BASE_URL}/users/${userId}`);
-  return userId
-}
+  console.log(`DELETE /api/users/${userId}`);
+
+  return { id: userId };
+};
