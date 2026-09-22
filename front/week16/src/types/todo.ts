afb3540c28cf095ddb386756129dfc0c1876693a
jiwoo choi <chlwldn418@naver.com>
2026-09-22 19:34:51 +0900

    feat: Todo 데이터 타입 정의

---
+++ front/week16/src/types/todo.ts
@@ -0,0 +1,9 @@
+export type TodoPriority = "low" | "medium" | "high";
+
+export type TodoFilter = "all" | "active" | "completed";
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
